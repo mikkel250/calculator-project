f88fbf53477994f56ec461741197557c8545832f
mikkel250 <mikkel250@gmail.com>
2018-02-14 17:08:12 -0800

    Added some comments to js file

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ var keys = document.querySelectorAll("#calculatorSurface button");
 var operators = [`+`, `-`, `*`, `/`];
 var decimalAdded = false;
 
-//add the onclick event to all keys
+// loop through all the keys because the querySelectorAll gives a nodelist and the onclick event must be added to each one
 for(let i = 0; i < keys.length; i++) {
-	// the e in this case is the event
+	// Add the onclick event to all keys. the e in this case is the event
 	keys[i].onclick = function(e) {
 		//get the input and button values
 		var input = document.querySelector('.screen');
@@ -19,10 +19,10 @@ for(let i = 0; i < keys.length; i++) {
 			// if equals button is pressed, calculate and display result
 		} else if(buttonVal == '=') {
 			// take everything in the screen and store it (as string) into the variable equation
-				var equation = inputVal;
-				var lastChar = equation[equation.length -1];
-				// check the last character of the equation and remove decimal or operator if present
+				var equation = inputVal;  // append equation to variable
+				var lastChar = equation[equation.length -1]; // target the end of the eval string
 				
+				// check the last character of the equation and remove decimal or operator if present				
 				if(operators.indexOf(lastChar) > -1 || lastChar == '.'){ equation = equation.replace(/.$/, "");	}
 				
 				if(equation){
@@ -34,11 +34,14 @@ for(let i = 0; i < keys.length; i++) {
 			// if backspace button pressed, replace the last char with empty char
 		} else if (buttonVal == 'Backspace') {
 			input.innerHTML = inputVal.replace(/.$/, "");
+			
 			// add some checks for user error
+
+			// Do not allow more than two operators to be added consecutively
 		} else if (operators.indexOf(buttonVal) > -1) {
 				// if operator is clicked last, get the last character from the equation instead
 				var lastChar = inputVal[inputVal.length -1];
-			
+			// only add operator if input is not empty
 				if(inputVal != "" && operators.indexOf(lastChar) == -1) {
 					input.innerHTML += buttonVal;
 				//allow minus if the input string is empty (for negative values)
@@ -53,12 +56,13 @@ for(let i = 0; i < keys.length; i++) {
 				
 		decimalAdded = false;
 			
-			// using the decimalAdded flag will prevent the use of more than one decimal per string
+			// add decimal. using the decimalAdded flag will prevent the use of more than one decimal per string
 		} else if(buttonVal == `.`) {
 				if(!decimalAdded) {
 				input.innerHTML += buttonVal;
 				decimalAdded = true;
 			}
+			// if any other key is pressed, just add it 
 		} else { 
 			input.innerHTML += buttonVal;
 		}
